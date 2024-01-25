b24fd486d20f55a5c63f4d48c8d178ac59cd04de
siva20456 <siva2002ismart2002@gmail.com>
2024-01-25 19:23:00 +0530

    ADDING APIs

--- src/components/ChatBot/MasterChefChatBot.tsx
+++ src/components/ChatBot/MasterChefChatBot.tsx
@@ -15,7 +15,7 @@ const ChatBot = () => {
         </div>
         <iframe
           title="Gradio App"
-          src="https://sivashankar-chef-0.hf.space"
+          src="Gradio" //https://sivashankar-chef-0.hf.space
           width="100%"
           height="500px"
           frameBorder="0"
